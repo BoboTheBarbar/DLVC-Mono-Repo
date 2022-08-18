368e5c4b6cd9bf9971c06240db09ac4826981ac6
b.prochnau <boris.prochnau@adesso.de>
2022-08-18 15:27:21 +0200

    build(setup): Initial Setup
    Creating intial setup

---
+++ src/main/kotlin/dlvc/manager/playerloader/PlayerLoaderApplication.kt
@@ -0,0 +1,11 @@
+package dlvc.manager.playerloader
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlayerLoaderApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlayerLoaderApplication>(*args)
+}
---
+++ src/test/kotlin/dlvc/manager/playerloader/PlayerLoaderApplicationTests.kt
@@ -0,0 +1,13 @@
+package dlvc.manager.playerloader
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlayerLoaderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
